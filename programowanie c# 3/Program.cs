91f4a8089f3209bb2137affa817ae5b4bf88184f
Oleksii Hazhii <oleksii.hazhii@microsoft.wsei.edu.pl>
2024-10-31 12:59:13 +0100

    c#

---
+++ programowanie c# 3/Program.cs
@@ -0,0 +1,67 @@
+﻿//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//
+//        //Main
+//        double x = double.Parse(Console.ReadLine());
+//        int n = int.Parse(Console.ReadLine());
+//
+//        Console.WriteLine(Power(x, n));
+//
+//    }
+     //Dodatki
+
+    /// <summary>
+    /// oblicza potenge x^n dla n nieujemnych
+    /// </summary>
+    /// <param name="x">dowolna liczba zamiennoprzecinkowa</param>
+    /// <param name="n">nieujemna liczba cialkowita</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+//    public static double Power(double x, int n)
+//    {
+//        if (n < 0)
+//        throw new ArgumentOutOfRangeException( "n" );
+//        double wynik = 1;
+//        for (int i = 0; i < n; i++)
+//        {
+//            wynik *= x;
+//        }
+//      return wynik;
+//    }
+
+
+//    public static int Max (int x , int y ) => (x < y) ? y : x;
+
+
+
+    //public static int Max(int a, int b, int c) => Max(Max(a, b), Max(a, c));
+
+
+
+    //    public static int Max(int a, int b, int c)
+    //    {
+    //        int max1 = max(a, b);
+    //        int max2 = max(a,c);
+    //        return max(max1, max2);
+    //
+    //        int max(int x, int y) => (x<y) ? y : x;
+    //        
+    //    }
+    //
+    //
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
